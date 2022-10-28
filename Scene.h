107d999a2bb8a155aef2c6d94bd3c939a296c0bd
Theo Dulka <dulka014@d.umn.edu>
2022-10-28 15:36:49 -0500

    Added basic window-edge collisions

--- Scene.h
+++ Scene.h
@@ -19,11 +19,16 @@ struct Circle : public sf::CircleShape {
 		return getPosition() + vec2{ getRadius(), getRadius() };
 	}
 
+	void setVel(const vec2& p_vel) {
+		vel = p_vel;
+	}
+
 	void move(const vec2& dpos) {
 		sf::CircleShape::move(dpos.x, dpos.y);
 	}
 };
 
+// Handles the circle objects: creating, updating, and drawing
 struct Scene {
 	float dt;
 	float timeElapsed = 0;
@@ -34,7 +39,9 @@ struct Scene {
 		: dt{ 1.0f / frameRate }, acc{ p_acc } {
 		float radius = 20.0f;
 		circles.push_back({ radius, {120,  20}, sf::Color::Green });
+		circles.back().setVel({ -200, 0 });
 		circles.push_back({ radius, {320, 180}, sf::Color::Blue });
+		circles.back().setVel({ 300, -80 });
 	}
 
 	void update(const sf::RenderWindow& window) {
@@ -43,6 +50,26 @@ struct Scene {
 		for (auto& c : circles) {
 			c.vel += acc * dt;
 			c.move(c.vel * dt);
+
+			vec2 min = c.getPos() - vec2{ c.getRadius(), c.getRadius() };
+			vec2 max = c.getPos() + vec2{ c.getRadius(), c.getRadius() };
+			if (min.x < 0) {
+				c.vel.x *= -1;
+				c.move({ -min.x, 0 });
+			}
+			if (min.y < 0) {
+				c.vel.y *= -1;
+				c.move({ 0, -min.y });
+			}
+			vec2 adjMax = max - vec2(window.getSize());
+			if (adjMax.y > 0) {
+				c.vel.y *= -1;
+				c.move({ 0, -adjMax.y });
+			}
+			if (adjMax.x > 0) {
+				c.vel.x *= -1;
+				c.move({ -adjMax.x, 0 });
+			}
 		}
 	}
 
